629c1d84a4f8c0e86c6a2aac860db8bf77136151
DanteBonino <43711055+DanteBonino@users.noreply.github.com>
2023-06-02 17:36:30 -0300

    Punto 3

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,59 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib () where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+--Datos que brinda el enunciado:
+--Los jugadores los definieron como un data
+martin = Jugador "Martin" 26 0.0 50 35.0
+juan = Jugador "Juancho" 30 0.2 50 40.0
+maxi = Jugador "Maxi Lopez" 27 0.4 68 30.0
+
+jonathan = Jugador "Chueco" 20 1.5 80 99.0
+lean = Jugador "Hacha" 23 0.01 50 35.0
+brian = Jugador "Panadero" 21 5 80 15.0
+
+garcia = Jugador "Sargento" 30 1 80 13.0
+messi =  Jugador "Pulga" 26 10 99 43.0
+aguero = Jugador "Aguero" 24 5 90 5.0
+
+--Los equipos como tuplas
+equipo1 = ("Lo Que Vale Es El Intento", 'F', [martin, juan, maxi])
+losDeSiempre = ( "Los De Siempre", 'F', [jonathan, lean, brian])
+restoDelMundo = ("Resto del Mundo", 'A', [garcia, messi, aguero])
+
+quickSort _ [] = [] 
+quickSort criterio (x:xs) = (quickSort criterio . filter (not . criterio x)) xs ++ [x] ++ (quickSort criterio . filter (criterio x)) xs 
+
+--Punto 1:
+data Jugador = Jugador{
+    nombre        :: String,
+    edad          :: Int,
+    promedioDeGol :: Float,
+    habilidad     :: Int,
+    cansancio     :: Float
+}
+type Equipo = (String, Char, [Jugador])
+
+figuras :: Equipo -> [Jugador]
+figuras = (soloFiguras . jugadores)
+
+jugadores :: Equipo -> [Jugador]
+jugadores (_,_, jugadoresDelEquipo) = jugadoresDelEquipo
+
+soloFiguras :: [Jugador] -> [Jugador]
+soloFiguras = filter (esFigura)
+
+esFigura :: Jugador -> Bool
+esFigura unJugador = ((>75) . habilidad) unJugador && ((>0) . promedioDeGol) unJugador
+
+--Punto 2:
+tieneFarandulero :: Equipo -> Bool
+tieneFarandulero = (algunoEsFarandulero . jugadores)
+
+algunoEsFarandulero :: [Jugador] -> Bool
+algunoEsFarandulero = any (flip elem jugadoresFaranduleros . nombre)
+
+jugadoresFaranduleros :: [String]
+jugadoresFaranduleros = ["Maxi Lopez", "Icardi", "Aguero", "Caniggia", "Demichelis"]
+
+
+
+
